20bc393f9772ea67a6e93fddb19f6b2e98fdb844
Roman Skokov <romainskokov@yandex.ru>
2024-12-08 20:58:51 +0300

    Second version

--- skra_test_bot.py
+++ skra_test_bot.py
@@ -1,8 +1,6 @@
-import pprint
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message, ContentType
-from aiogram import F
+from aiogram.types import Message
 
 # There is a bot's name
 BOT_TOKEN = '8135283680:AAFH31-AWM-3tLbQ0LaFQNZ4P-QEhDQ2j4E'
@@ -13,11 +11,13 @@ dp = Dispatcher()
 
 
 # Create handler that will be work by use command /start
+@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши что-нибудь :)')
 
 
 # Create handler that will be work by use command /help
+@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши че нить\n'
@@ -26,28 +26,16 @@ async def process_help_command(message: Message):
 
 
 # Create handler that will work with any message beside '/start' and '/help'
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
-
-
-# Create handler that will work with photo_type message
-async def send_photo_echo(message: Message):
-    await message.reply_photo(message.photo[0].file_id)
-
-
-# Create handler that will work with sticker_type
-async def send_sticker_echo(message: Message):
-    pprint.pprint(message)
-    await message.reply_sticker(message.sticker.file_id)
-
-
-# Register our handlers
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_sticker_echo, F.sticker)
-dp.message.register(send_echo)
-
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+            'метод send_copy'
+        )
 
 if __name__ == '__main__':
     dp.run_polling(bot)
